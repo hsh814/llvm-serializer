4e9a1b3a4c33d4cfea6f02ed5b465e34b1fbde0d
hsh814 <shhan@unist.ac.kr>
2024-11-18 07:26:31 +0000

    Read functions

--- main.cpp
+++ main.cpp
@@ -17,10 +17,11 @@
 
 using namespace llvm;
 
-std::unique_ptr<llvm::Module> parseIRFile(const std::string &fileName) {
-  llvm::LLVMContext context;
+std::unique_ptr<llvm::Module> parseIRFile(const std::string fileName,
+                                          llvm::LLVMContext &context) {
   llvm::SMDiagnostic err;
-  std::unique_ptr<llvm::Module> module = parseIRFile(fileName, err, context);
+  std::unique_ptr<llvm::Module> module =
+      llvm::parseIRFile(fileName, err, context);
   if (!module) {
     err.print(fileName.c_str(), errs());
     return nullptr;
@@ -28,23 +29,34 @@ std::unique_ptr<llvm::Module> parseIRFile(const std::string &fileName) {
   return module;
 }
 
-void printModuleInfo(std::unique_ptr<llvm::Module> &module,
-                     std::string &targetFunctionName) {
+void printArgTypeInfo(std::unique_ptr<llvm::Module> &module,
+                      std::string targetFunctionName) {
   llvm::Function *targetFunction = module->getFunction(targetFunctionName);
   if (!targetFunction) {
     errs() << "Function not found: " << targetFunctionName << "\n";
     return;
   }
+  for (auto &arg : targetFunction->args()) {
+    std::string argName = arg.getName().str();
+    llvm::Type *argType = arg.getType();
+    std::string argTypeName;
+    llvm::raw_string_ostream rso(argTypeName);
+    argType->print(rso);
+    rso.str();
+    std::cout << "Argument name: " << argName << ", type: " << argTypeName
+              << "\n";
+  }
 }
 
 int main(int argc, char **argv, char **envp) {
-  if (argc < 2) {
-    errs() << "Usage: " << argv[0] << " <IR file>\n";
+  if (argc < 3) {
+    errs() << "Usage: " << argv[0] << " <IR file> <Target function>\n";
     return 1;
   }
-  std::unique_ptr<llvm::Module> module = parseIRFile(argv[1]);
+  llvm::LLVMContext context;
+  std::unique_ptr<llvm::Module> module = parseIRFile(argv[1], context);
   if (module) {
-    module->print(outs(), nullptr);
+    printArgTypeInfo(module, argv[2]);
   } else {
     errs() << "Failed to parse IR file\n";
     return 1;
